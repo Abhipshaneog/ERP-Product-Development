457dbc8ba368914e1df00b1152b956e2785bd556
Abhipsha Neog <abhipshaneog0381@gmail.com>
2024-12-31 11:42:06 +0530

    modified:   Frontend/src/App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./components/navbar";
-import Menu from "./components/Menu";
-import MenuSecond from "./components/MenuSecond";
-import Hero from "./components/Hero";
-import FeaturedComponent from "./components/FeaturedComponent";
-import FeaturedCategories from "./components/FeaturedCategories";
-import ProductLandingPage from "./components/ProductLandingPage";
-import Footer from "./components/footer";
-import EcommerceSolution from "./components/EcommerceSolution";
-import Newsletter from "./components/Newsletter";
+import EcommerceSolution from "./components/HomePage/EcommerceSolution";
+import FeaturedCategories from "./components/HomePage/FeaturedCategories";
+import FeaturedComponent from "./components/HomePage/FeaturedComponent";
+import Hero from "./components/HomePage/Hero";
+import Menu from "./components/HomePage/Menu";
+import MenuSecond from "./components/HomePage/MenuSecond";
+import Newsletter from "./components/HomePage/Newsletter";
+import ProductLandingPage from "./components/HomePage/ProductLandingPage";
+import Footer from "./components/HomePage/footer";
+import Navbar from "./components/HomePage/navbar";
 
 function App() {
   return (
