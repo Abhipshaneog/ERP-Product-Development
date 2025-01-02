d48f84453aa36e5c9a5962fbc83c5653565f7bc6
SwarnadeepDeb <swarnadeepdebdevelopment@gmail.com>
2025-01-02 10:35:54 +0530

    added contact us page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
+
 import HomePageIndex from "./components/HomePage/HomePageIndex";
 import AboutIndex from "./components/AboutUsPage/AboutIndex";
+import ContactIndex from "./components/ContactUsPage/ContactIndex";
+
 import Layout from "./Layout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -12,6 +15,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePageIndex />} />
           <Route path="about" element={<AboutIndex />} />
+          <Route path="contact" element={<ContactIndex />} />
         </Route>
       </Routes>
     </Router>
---
+++ Frontend/src/components/ContactUsPage/ContactIndex.jsx
@@ -0,0 +1,14 @@
+import ContactInfo from "./ContactInfo";
+import ContactSupport from "./ContactSupport";
+import ContactUsHero from "./ContactUsHero";
+function AboutIndex() {
+  return (
+    <>
+    <ContactSupport/>
+    <ContactInfo/>
+    <ContactUsHero/>
+    </>
+  );
+}
+
+export default AboutIndex;
