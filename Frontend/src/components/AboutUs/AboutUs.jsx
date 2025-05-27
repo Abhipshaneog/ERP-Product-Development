4ace34a799cdce71543bb6be87e68d615f2c22a3
abhishek83568 <abhishek83568@gmail.com>
2025-05-27 13:27:41 +0530

    changes in About page

--- Frontend/src/components/AboutUs/AboutUs.jsx
+++ Frontend/src/components/AboutUs/AboutUs.jsx
@@ -1,17 +1,17 @@
-import './AboutUs.css';
-import DeveloperSection from './DeveloperSection';
-import GallerySection from './GallerySection';
-import HeaderSection from './HeaderSection';
-import StoreSection from './StoreSection';
-import TeamSection from './TeamSection';
+import "./AboutUs.css";
+import DeveloperSection from "./DeveloperSection";
+import GallerySection from "./GallerySection";
+import HeaderSection from "./HeaderSection";
+import StoreSection from "./StoreSection";
+import TeamSection from "./TeamSection";
 
 const AboutUs = () => (
   <div className="about-us-page">
     <HeaderSection />
-    <StoreSection />
+    {/* <StoreSection />
     <TeamSection />
     <DeveloperSection />
-    <GallerySection />
+    <GallerySection /> */}
   </div>
 );
 
