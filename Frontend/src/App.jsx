bfef2ec9a1e0aac80e30cd5765f7e2fe67d8dbf3
abhishek83568 <abhishek83568@gmail.com>
2025-06-09 12:25:26 +0530

    frontend codes added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,7 +18,7 @@ import OrderConfirmation from "./components/OrderConfirmation/OrderConfirmation"
 import CancelOrderPage from "./components/OrderHistory/CancelOrderPage";
 import OrderPage from "./components/OrderHistory/OrderPage";
 import TrackOrderPage from "./components/OrderHistory/TrackOrderPage";
-import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
+// import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import ShoppingPageIndex from "./components/ShoppingPage/ShoppingPageIndex";
@@ -44,39 +44,44 @@ function App() {
   });
   return (
     <>
-    <CartProvider> 
-      <Router>
-      
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomePage />} />
-            <Route path="about" element={<AboutUs />} />
-            <Route path="contact" element={<ContactUs />} />
-            <Route path="my-account" element={<LoginRegister />} />
-            <Route path="password-reset" element={<PasswordReset />} />
-            <Route path="reset-password/:token" element={<ResetPassword />} />
-            <Route path="shopping-page" element={<ShoppingPageIndex />} />
-            <Route path="product-details/:id" element={<ProductDetails />} />
-            <Route path="cart/:user_id" element={<CartPage />} />
-            <Route path="wishlist" element={<Wishlist />} />
-           <Route path="/checkout/:user_id" element={<CheckoutPageMain />} />
-            <Route path="order-confirmation" element={<OrderConfirmation />} />
-            <Route path="orders" element={<OrderPage />} />
+      <CartProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+              <Route path="about" element={<AboutUs />} />
+              <Route path="contact" element={<ContactUs />} />
+              <Route path="my-account" element={<LoginRegister />} />
+              <Route path="password-reset" element={<PasswordReset />} />
+              <Route path="reset-password/:token" element={<ResetPassword />} />
+              <Route path="shopping-page" element={<ShoppingPageIndex />} />
+              <Route path="product-details/:id" element={<ProductDetails />} />
+              <Route path="cart/:user_id" element={<CartPage />} />
+              <Route path="wishlist" element={<Wishlist />} />
+              <Route path="/checkout/:user_id" element={<CheckoutPageMain />} />
+              <Route
+                path="order-confirmation"
+                element={<OrderConfirmation />}
+              />
+              <Route path="orders" element={<OrderPage />} />
               {/*  <Route path="/orders/:orderId/items/:orderItemId/track" element={<TrackOrderPage />} />
               <Route path="/orders/:orderId/items/:orderItemId/cancel" element={<CancelOrderPage />} />
               */}
               <Route path="track" element={<TrackOrderPage />} />
               <Route path="cancel" element={<CancelOrderPage />} />
-            
-            <Route path="privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="faq" element={<FAQ />} />
-            <Route path="terms-and-conditions" element={<TermsAndConditions />} />
-            <Route path="profile" element={<ProfilePage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-        <ToastContainer position="top-right" autoClose={3000} />
-      </Router>
+              {/*             
+            <Route path="privacy-policy" element={<PrivacyPolicy />} /> */}
+              <Route path="faq" element={<FAQ />} />
+              <Route
+                path="terms-and-conditions"
+                element={<TermsAndConditions />}
+              />
+              <Route path="profile" element={<ProfilePage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+          <ToastContainer position="top-right" autoClose={3000} />
+        </Router>
       </CartProvider>
     </>
   );
